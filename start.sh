4d67991347333aa00e5a49946d85b542e2dd29fa
liberjink960 <maaztkhan@outlook.com>
2026-03-18 18:00:48 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Velocity uses Java 21
 cd velocity
 java -Xms512M -Xmx1G -jar velocity.jar &
 cd ..
 
 sleep 5
 
+# Paper 1.8.8 uses Java 8
 cd backend-1.8.8
-java -Xms1G -Xmx2G -jar paper-1.8.8.jar nogui &
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xms1G -Xmx2G -jar paper-1.8.8.jar nogui &
 cd ..
 
 sleep 5
 
+# Paper 1.12.2 uses Java 8
 cd backend-1.12.2
-java -Xms1G -Xmx2G -jar paper-1.12.2.jar nogui &
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xms1G -Xmx2G -jar paper-1.12.2.jar nogui &
 cd ..
 
 wait
